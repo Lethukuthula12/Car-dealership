0d7029ed797c856ffe4694eae214bcc874d5826b
Lethukuthula <u17241414@tuks.co.za>
2020-10-09 10:15:54 +0200

    I moved our shop data from component into its reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/Components/pages/shop/shop.component.jsx
+++ src/Components/pages/shop/shop.component.jsx
@@ -1,28 +1,19 @@
 //because we have to store data in shop components then we going to use a class component
+import React from "react";
+import {Route} from "react-router-dom";
+import CollectionPage from "../collection/collection.component";
+import CollectionOverview from "../../collections-overview/collection-overview.component"
 
-import React, { Component } from "react";
-import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../preview-component/collection-preview"
-
-class ShopPage extends Component{
-    constructor(props){
-        super(props);
-
-
-        this.state = { collections: SHOP_DATA };
-    };
-
-
-    render(){
-        const {collections} = this.state;
-        return(<div className="shop-page">
-              {
-                  collections.map(({id, ...otherCollectionProps}) =>(
-                  <CollectionPreview key={id} {...otherCollectionProps} />))
-             }
-            </div>
-        );
-    }
+const ShopPage = ({ match }) => {
+  console.log(match);
+  return(<div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionOverview} />
+    
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>)
 }
+    
+
+
 
 export default ShopPage;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux'; /* provider is there to make sure that all the components in our app gets a access to our redux store by rapping around all our app */
-import store from "./redux/store";
+
+import {PersistGate} from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
-  <Provider store = {store}> 
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
