315df2da5f18c9052c58c0e18f0ba4b3b795d6bd
Lethukuthula <u17241414@tuks.co.za>
2020-10-09 15:38:20 +0200

    i added a stripe intergation for payments and updated a title of my project

--- src/Components/pages/checkout/checkout.components.jsx
+++ src/Components/pages/checkout/checkout.components.jsx
@@ -5,6 +5,7 @@ import {createStructuredSelector} from "reselect"
 import  {selectCartItems, selectCartTotal} from "../../../redux/cart/cart.selectors"
 import "./checkout.styles.scss";
 import CheckoutItem from "../../checkout-item/checkout-item.component"
+import StripeCheckoutButton  from "../../stripe-button/stripe-button.component"
 
 
 const CheckOutPage = ({cartItems, total}) => (
@@ -34,8 +35,14 @@ const CheckOutPage = ({cartItems, total}) => (
     }
 
     <div className="total"><span>TOTAL: ${total}</span></div>
+    <div className="test-warning">
+    *Please use the following test credit card for payments <br/>
+    4242 4242 4242 4242 -Exp:01/21 - CVV:123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
+
 
 const mapStateToProps = createStructuredSelector({
     cartItems : selectCartItems,
---
+++ src/Components/stripe-button/stripe-button.component.js
@@ -0,0 +1,31 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+
+const StripeCheckoutButton = ({price})=>{
+   const priceForStripe =  price * 100;
+   const publishableKey =
+     "pk_test_51HaH5mIBlFkwVZwmzYIX6bFRZdTuYyrNnRKYVOLmC2dFWMcnESQa0AyIm8eMcI6FVs2YTAkEhXgvixve7myXqk0m00qV8gjyAD";
+
+     const onToken = token =>{
+       console.log(token);
+       alert("Payment Successful")
+     }
+
+     return (
+       <StripeCheckout 
+       label ="PAY NOW"
+       name ="Clothing Ltd"
+       billingAddress
+       shippingAddress
+       image ="https://svgshare.com/i/CUz.svg"
+       description={`Your total is R${price}`}
+       amount ={priceForStripe}
+       panelLabel ="Pay Now"
+       token={onToken}
+       stripeKey ={publishableKey}
+        />
+     )
+}
+
+export default StripeCheckoutButton;
